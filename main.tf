13158c83eb97165d5db50866678e60d8d2a78662
jitendra kumar bansal <40315203+Jitendral@users.noreply.github.com>
2025-07-07 13:09:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,32 +1,21 @@
-# main.tf
+terraform {
+  required_version = ">= 1.3.0"
 
-resource "aws_ecr_repository" "node_app" {
-  name = var.repository_name
-}
-
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
-  cluster_version = "1.24"
-  subnets         = module.vpc.private_subnets
-  vpc_id          = module.vpc.vpc_id
-
-  manage_aws_auth = true
-
-  node_groups = {
-    default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-
-      instance_types = ["t3.medium"]
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
+provider "aws" {
+  region = var.aws_region
+}
+
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.5.0"
+  version = "5.1.1"
 
   name = "eks-vpc"
   cidr = "10.0.0.0/16"
@@ -37,4 +26,28 @@ module "vpc" {
 
   enable_nat_gateway = true
   single_nat_gateway = true
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.4"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.24"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+    }
+  }
+}
+
+resource "aws_ecr_repository" "node_app" {
+  name = var.repository_name
 }
